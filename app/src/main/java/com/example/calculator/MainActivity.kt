2c66d44b20e7b488325c551f68900f4efeb52a8a
Oleksii Sukhenko <sukhenko.oleksii@gmail.com>
2023-03-30 09:59:13 +0300

    added rest of buttons

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -31,10 +31,96 @@ class MainActivity : AppCompatActivity() {
 
         val numberStringBuilder = StringBuilder()
 
+        zeroButton.setOnClickListener {
+
+            numberStringBuilder.append(0)
+            resultTextView.text = numberStringBuilder
+        }
+
         oneButton.setOnClickListener {
 
             numberStringBuilder.append(1)
             resultTextView.text = numberStringBuilder
         }
+
+        twoButton.setOnClickListener {
+
+            numberStringBuilder.append(2)
+            resultTextView.text = numberStringBuilder
+        }
+
+        threeButton.setOnClickListener {
+
+            numberStringBuilder.append(3)
+            resultTextView.text = numberStringBuilder
+        }
+
+        fourButton.setOnClickListener {
+
+            numberStringBuilder.append(4)
+            resultTextView.text = numberStringBuilder
+        }
+
+        fiveButton.setOnClickListener {
+
+            numberStringBuilder.append(5)
+            resultTextView.text = numberStringBuilder
+        }
+
+        sixButton.setOnClickListener {
+
+            numberStringBuilder.append(6)
+            resultTextView.text = numberStringBuilder
+        }
+
+        sevenButton.setOnClickListener {
+
+            numberStringBuilder.append(7)
+            resultTextView.text = numberStringBuilder
+        }
+
+        eightButton.setOnClickListener {
+
+            numberStringBuilder.append(8)
+            resultTextView.text = numberStringBuilder
+        }
+
+        nineButton.setOnClickListener {
+
+            numberStringBuilder.append(9)
+            resultTextView.text = numberStringBuilder
+        }
+
+
+        pointButton.setOnClickListener {
+
+            numberStringBuilder.append(".")
+            resultTextView.text = numberStringBuilder
+        }
+
+        plusButton.setOnClickListener {
+
+            numberStringBuilder.append("+")
+            resultTextView.text = numberStringBuilder
+        }
+
+        minusButton.setOnClickListener {
+
+            numberStringBuilder.append("-")
+            resultTextView.text = numberStringBuilder
+        }
+
+        multiplyButton.setOnClickListener {
+
+            numberStringBuilder.append("*")
+            resultTextView.text = numberStringBuilder
+        }
+
+        equalButton.setOnClickListener {
+
+            numberStringBuilder.append("=")
+            resultTextView.text = numberStringBuilder
+        }
+
     }
 }
